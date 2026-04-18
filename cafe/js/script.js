c72040402cbd99765f1d8287726cb0d9f33e8018
ueno <ue2768308@gmail.com>
2026-04-18 22:39:16 +0900

    indexのデザイン

--- cafe/js/script.js
+++ cafe/js/script.js
@@ -52,13 +52,41 @@ $('#slider').vegas({
 		//timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
 	});
 
-// スクロール
-$(function(){
-  $(".inview_re").on("inview", function (event, isInView) {
-    if (isInView) {
-      $(this).stop().addClass("is-show");
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+//ふわっと動くきっかけのクラス名と動きのクラス名の設定
+$('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+  var elemPos = $(this).offset().top - 50; //要素より、50px上の
+  var scroll = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  if (scroll >= elemPos - windowHeight) {
+    $(this).addClass('fadeUp');
+    // 画面内に入ったらfadeInというクラス名を追記
+  } else {
+    $(this).removeClass('fadeUp');
+    // 画面外に出たらfadeInというクラス名を外す
+  }
+});
+
+  //関数でまとめることでこの後に違う動きを追加することが出来ます
+  $('.fadeDownTrigger').each(function () { //fadeInDownTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeDown');
+      // 画面内に入ったらfadeDownというクラス名を追記
     } else {
-      $(this).stop().removeClass("is-show");
+      $(this).removeClass('fadeDown');
+      // 画面外に出たらfadeDownというクラス名を外す
     }
   });
-});
+
+
+}//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
